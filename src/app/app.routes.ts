43ff79f5e63405282225a55feac49f863495b370
Isabella Rodriguez <irodriguezme@unbosque.edu.co>
2026-05-17 14:18:34 -0500

    Preguntas terminadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,14 +12,6 @@ import { ComoJugar } from './component/comoJugar/comoJugar';
 import { ConocerMas } from './component/conocer-mas/conocer-mas';
 import { authGuard } from './service/auth.guard';
 import { Questions } from './component/questions/questions';
-import { All } from './component/game/all/all';
-import { CallingCodes } from './component/game/calling-codes/calling-codes';
-import { Capitals } from './component/game/capitals/capitals';
-import { Continents } from './component/game/continents/continents';
-import { Currencies } from './component/game/currencies/currencies';
-import { Flags } from './component/game/flags/flags';
-import { Language } from './component/game/language/language';
-import { Population } from './component/game/population/population';
 
 export const routes: Routes = [
   {
@@ -27,7 +19,9 @@ export const routes: Routes = [
     component: Login,
   },
   {
-    path: '', redirectTo: '/login', pathMatch: 'full'
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
   },
 
   {
@@ -41,79 +35,44 @@ export const routes: Routes = [
   {
     path: 'home',
     component: Home,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     path: 'modo',
     component: Modo,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     path: 'libre',
     component: Libre,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     path: 'aprendizaje',
     component: Aprendizaje,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     path: 'perfil',
     component: Perfil,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     path: 'comojugar',
     component: ComoJugar,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
   {
     path: 'conocermas',
     component: ConocerMas,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'questions/:id',
-    component: Questions,
     canActivate: [authGuard],
   },
-  {
-    path: 'all',
-    component: All,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'calling-codes',
-    component: CallingCodes,
-    canActivate: [authGuard]
-  },  {
-    path: 'capitals',
-    component: Capitals,
-    canActivate: [authGuard]
-  },  {
-    path: 'continents',
-    component: Continents,
-    canActivate: [authGuard]
-  },  {
-    path: 'currencies',
-    component: Currencies,
-    canActivate: [authGuard]
-  },  {
-    path: 'flags',
-    component: Flags,
-    canActivate: [authGuard]
-  },  {
-    path: 'language',
-    component: Language,
-    canActivate: [authGuard]
-  },
-  {
-    path: 'population',
-    component: Population,
-    canActivate: [authGuard]
-  },
-
+  { path: 'questions/categoria/:id',
+    component: Questions,
+    canActivate: [authGuard] },
+  { path: 'questions/dificultad/:nivel',
+    component: Questions,
+    canActivate: [authGuard] },
   {
     path: '**',
     redirectTo: '',
